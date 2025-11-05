2ff5c91296b27aa68928392dd0cfea88c1c7faea
chinthaka-dinadasa <sendtoclabz@gmail.com>
2025-11-06 00:58:23 +0530

    added router GIN

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Print("Hello World")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		// Return JSON response
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
 }
