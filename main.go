683f4f8671c2ad63e01cf688bebae2e2c27bef41
chinthaka-dinadasa <sendtoclabz@gmail.com>
2025-11-09 11:14:57 +0530

    https

--- main.go
+++ main.go
@@ -2,17 +2,44 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+type WebsiteAnalysingRequest struct {
+	Url string `json:"url" binding:"required"`
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		// Return JSON response
+
+	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"health":      "up",
+			"api":         "web-analyser-api-v1.0",
+			"requestedOn": time.Now().Format(time.RFC3339),
 		})
 	})
+
+	router.POST("/process-web-url", processWebUrl)
+	// router.GET("/check/status", checkStatus)
+
 	router.Run()
+}
+
+func processWebUrl(c *gin.Context) {
+	var webAnylysingRequest WebsiteAnalysingRequest
+	// Bind JSON to struct with validation
+	if err := c.ShouldBindJSON(&webAnylysingRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	// Process user data
+	c.JSON(http.StatusCreated, gin.H{
+		"status": "processing",
+		"weburl": webAnylysingRequest,
+	})
 }
