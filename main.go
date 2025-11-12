aa24033ec360115e9879985aebeb5d2337f8c4a5
chinthaka-dinadasa <sendtoclabz@gmail.com>
2025-11-12 22:10:50 +0530

    added cors default with gin to support fe requests.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"web-analyser/handlers"
 	"web-analyser/services"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,7 +14,7 @@ func main() {
 	router := gin.Default()
 	analyser := services.NewAnalyserService()
 	analyseHandler := handlers.NewAnalyseHandler(analyser)
-
+	router.Use(cors.Default())
 	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"health":      "up",
