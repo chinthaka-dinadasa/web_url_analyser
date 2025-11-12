781256009f72c17cbd556be4e8ad27d5eaefabfb
chinthaka-dinadasa <sendtoclabz@gmail.com>
2025-11-12 22:02:39 +0530

    ui done

--- frontend/web-analyser-web/app/page.tsx
+++ frontend/web-analyser-web/app/page.tsx
@@ -1,7 +1,11 @@
+'use client'
 import { useState } from "react";
 
 export default function Home() {
   const [url, setUrl] = useState('')
+  const handleSubmit = async (e: React.FormEvent) => {
+
+  }
   return (
     <div className="min-h-screen bg-white py-8">
       <div className="max-w-4xl mx-auto px-4">
@@ -10,6 +14,31 @@ export default function Home() {
           <p className="text-gray-600">
             Analyze any website's with golang API
           </p>
+        </div>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="url" className="block text-sm font-medium text-gray-700 mb-2">
+                Website URL
+              </label>
+              <input
+                type="url"
+                id="url"
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+                placeholder="https://example.com"
+                className="w-full px-3 py-2 border border-gray-300"
+              />
+             <div className="flex gap-20">
+ <button
+                type="submit"
+                className="flex-1 bg-blue-600 text-white py-2 px-4"
+              >
+                  Analyze Website
+              </button>
+             </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
