9ad9fcd0d4949fb1afdc7bdaf5b454fcdde3c5cb
chinthaka-dinadasa <sendtoclabz@gmail.com>
2025-11-12 21:29:23 +0530

    added sync mutex to store data with cache

--- services/cache.go
+++ services/cache.go
@@ -1,15 +1,34 @@
 package services
 
 import (
+	"sync"
 	"web-analyser/models"
 )
 
 type SimpleCache struct {
-	data map[string]*models.WebAnalysingResponse
+	data  map[string]*models.WebAnalysingResponse
+	mutex sync.RWMutex
 }
 
 func NewSimpleCache() *SimpleCache {
 	return &SimpleCache{
 		data: make(map[string]*models.WebAnalysingResponse),
 	}
+}
+
+func (c *SimpleCache) Get(url string) *models.WebAnalysingResponse {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+
+	if result, exists := c.data[url]; exists {
+		return result
+	}
+	return nil
+}
+
+func (c *SimpleCache) Set(url string, result *models.WebAnalysingResponse) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.data[url] = result
 }
