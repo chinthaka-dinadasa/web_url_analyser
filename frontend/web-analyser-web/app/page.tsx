d66b7d142fe9bc1953512c3e81f2a3c365949fc6
chinthaka-dinadasa <sendtoclabz@gmail.com>
2025-11-12 21:57:12 +0530

    ui

--- frontend/web-analyser-web/app/page.tsx
+++ frontend/web-analyser-web/app/page.tsx
@@ -1,11 +1,15 @@
-import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [url, setUrl] = useState('')
   return (
-    <div className="min-h-screen py-8">
+    <div className="min-h-screen bg-white py-8">
       <div className="max-w-4xl mx-auto px-4">
         <div className="text-center mb-8">
-          <h1>Web Analyser - Golang Web UI</h1>
+          <h1 className="text-3xl font-bold text-gray-900 mb-2">Web Analyser - Golang Web UI</h1>
+          <p className="text-gray-600">
+            Analyze any website's with golang API
+          </p>
         </div>
       </div>
     </div>
